25344dba054e24d829146c688fcf9352e2fa6cd3
Abhishek Mahesh Pandey <pandeya@usc.edu>
2018-06-26 10:22:15 -0700

    Lambda works

--- src/main/java/com/abhishektweet/serverless/tweetsentiment/TweetSentiment.java
+++ src/main/java/com/abhishektweet/serverless/tweetsentiment/TweetSentiment.java
@@ -1,11 +1,22 @@
 package com.abhishektweet.serverless.tweetsentiment;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 
 //Core Tweet Sentiment Logic
+@Slf4j
 public class TweetSentiment {
+    private static final Logger logger = LoggerFactory.getLogger(TweetSentiment.class);
 
-    public void publishSentimentMetrics(List<String> tweets) {
+    public void publishSentimentMetrics(List<String> tweetStrings) {
+        //log.info("Received Tweets: {}",tweetStrings);
+        //System.out.println(tweetStrings);
+        //log.info();
+        logger.info("Testing 123");
+
 
     }
 }
